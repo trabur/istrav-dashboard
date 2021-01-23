621c8e9d83060af7b39d8c4dbe8815b6f0ac95b6
trabur <travis.burandt@gmal.com>
2021-01-22 22:06:38 -0600

    nav

--- components/my-layout.js
+++ components/my-layout.js
@@ -1,21 +1,89 @@
 const template = document.createElement('template');
 
 template.innerHTML = /*html*/`
-<nav>
-  <div class="nav-wrapper">
-    <a href="#" class="brand-logo">Logo</a>
-    <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">JavaScript</a></li>
-    </ul>
-  </div>
-</nav>
-<main>
-  <slot>
-    <div></div>
-  </slot>
-</main>
+<style>
+  .expand {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  .masonry {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 0.3fr 1fr;
+  }
+
+  .item {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    overflow: auto;
+  }
+
+	.sidebar {
+		color: #333;
+		max-width: 300px;
+		margin: 0;
+		background: #eee;
+		height: 100%;
+		padding: 0 1em;
+		position: relative;
+		overflow: auto;
+	}
+
+	main {
+		color: #ccc;
+		position: relative;
+		width: 100%;
+		margin: 0; 
+		padding: 0; 
+		background: #111;
+	}
+
+	.sidebar a {
+		text-decoration: underline;
+	}
+</style>
+<div class="masonry expand">
+	<div class="item">
+		<br />
+		<br />
+		<br />
+		<div style="border-bottom: 5px dashed;">
+			<h1 style="text-align: center; font-size: 2em;">ISTRAV</h1>
+		</div>
+		<br />
+		<br />
+    <h3 style="text-align: center; margin: 0 0 0.2em 0; font-size: 1.5em">DASHBOARD</h3>
+		<ul style="font-size: 1.4em; font-family: monospace; list-style-type: none; text-align: center; margin: 0; padding: 0;">
+      <li><a href="/account.users">account.users()</a></li>
+      <li><a href="/account.cart">account.cart()</a></li>
+      <li><a href="/account.orders">account.orders()</a></li>
+      <li><a href="/fleet.vehicles">fleet.vehicles()</a></li>
+      <li><a href="/fleet.employees">fleet.employees()</a></li>
+			<li><a href="/fleet.customers">fleet.customers()</a></li>
+			<li><a href="/fleet.locations">fleet.locations()</a></li>
+			<li><a href="/store.featured">store.featured()</a></li>
+			<li><a href="/store.categories">store.categories()</a></li>
+			<li><a href="/store.collections">store.collections()</a></li>
+			<li><a href="/store.products">store.products()</a></li>
+		</ul>
+		<br />
+		<br />
+		<div style="border-top: 5px dashed;">
+			<h1 style="text-align: center; font-size: 2em;">ISTRAV</h1>
+		</div>
+		<br />
+		<br />
+		<br />
+	</div>
+	<main class="item">
+		<slot><div></div></slot>
+	</main>
+</div>
 `
 
 //my-layout.js
