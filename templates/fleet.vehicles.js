9a1e5a4db9a21aaaec31a8fe5824e4943b3eb4ed
trabur <travis.burandt@gmal.com>
2021-01-24 00:42:38 -0600

    deploy

--- templates/fleet.vehicles.js
+++ templates/fleet.vehicles.js
@@ -1,10 +1,17 @@
-import * as istrav from "../node_modules/istrav-library/api/index.js";
+import { istrav } from "../node_modules/istrav/api/index.js";
 
 export let fleetVehiclesTemplate = /*html*/`
 <p>hello fleet</p>
 `
 
-export function fleetVehicles () {
+export async function fleetVehicles () {
 	console.log('fleet')
-	console.log('istrav', istrav)
+	let users = istrav.account.users
+
+	users.init({
+		host: 'https://api.istrav.com'
+	})
+
+	let data = await users.all()
+	console.log(JSON.stringify(data, null, 2))
 }
