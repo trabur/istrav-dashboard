26a608f7b5bfacb417810fd23b8d78b0f989edab
trabur <travis.burandt@gmal.com>
2021-01-23 22:53:00 -0600

    express

---
+++ server.js
@@ -0,0 +1,27 @@
+// service
+import express from "express"
+const app = express()
+const port = process.env.PORT || 3000
+
+// get dirname working in repl
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// dashboard navigation
+app.get([
+  '/fleet-vehicles',
+  '/account-users'
+], function(req, res) {
+  res.sendFile('./index.html', { root: __dirname });
+})
+
+// dashboard default
+app.get('/*', express.static('.'))
+
+// run
+app.listen(port, () => {
+  console.log("Running server on port:", port)
+  console.log("--------------------------")
+});
