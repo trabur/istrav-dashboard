72b86beee722cf7275963d8af619a542722ae9f0
trabur <travis.burandt@gmal.com>
2021-01-30 17:16:31 -0600

    scripts and documentation

--- templates/dashboard.js
+++ templates/dashboard.js
@@ -19,18 +19,21 @@ export let dashboardTemplate = /*html*/`
     <a class="waves-effect waves-light btn" onclick="window.doRun('doRun'); return false;">run</a>
   </div>
   <h3>> console.log() outputs to the right!</h3>
-  <p>> notice: when we run each script in the dashboard our logging shows up on the right ... all we need todo is code "console.log('welcome to istrav')", click run, and then "welcome to istrav" will show up.</p>
-  <p>the entire project is open source. you can clone all of our code and run each project by yourself; that is: if you know what your doing. although, i'm updating the code daily which would make constantly deploying to production a hassle therefore it's built with multi tenant in mind. i also have 10 or so other websites that i want to integrate without having to redeploy all kinds of other code and _blank_ as a service 3rd party software.</p>
+  <p>notice: when we run each script in the dashboard our logging shows up on the right ... all we need todo is code "console.log('welcome to istrav')", click run, and then "welcome to istrav" will show up.</p>
+
+  <h3>> code is free and open source!</h3>
+  <p>the entire project is free and open source. you can clone all of our code and run each project by yourself; that is: if you know what your doing. although, i'm updating the code daily which would make constantly deploying to production a hassle therefore it's built with multi tenant in mind. i also have 10 or so other websites that i want to integrate without having to redeploy all kinds of other code and _blank_ as a service 3rd party software.</p>
   <div style="text-align: center;">
+    <p>(give one or all projects a like to show your support!)</p>
     <a href="https://github.com/trabur/istrav-api" target="_blank" class="waves-effect waves-light btn">github.com/trabur/istrav-api</a><br /><br />
     <a href="https://github.com/trabur/istrav-channels" target="_blank" class="waves-effect waves-light btn">github.com/trabur/istrav-channels</a><br /><br />
     <a href="https://github.com/trabur/istrav-dashboard" target="_blank" class="waves-effect waves-light btn">github.com/trabur/istrav-dashboard</a><br /><br />
     <a href="https://github.com/trabur/istrav-library" target="_blank" class="waves-effect waves-light btn">github.com/trabur/istrav-library</a><br /><br />
     <a href="https://github.com/trabur/istrav-storefront" target="_blank" class="waves-effect waves-light btn">github.com/trabur/istrav-storefront</a><br /><br />
-    <p>(give one or all projects a like to show your support!)</p>
   </div>
+  <h3>> services used and offered:</h3>
   <p>speaking of 3rd party software here is a list of products that i use just to keep the lights on. note: all these services have free plans available however if i start getting use out of these endpoints i'll have to start paying and ultimatly having to charge subscribers.</p>
-  <p>services involved:</p>
+  <p>project requirments:</p>
   <ul>
     <li>aws.amazon.com // route53 domain name</li>
     <li>cloudflare.com // dns</li>
@@ -47,6 +50,7 @@ export let dashboardTemplate = /*html*/`
     <li><a href="https://dashboard.istrav.com" target="_blank">https://dashboard.istrav.com</a> // admin</li>
     <li><a href="https://channels.istrav.com" target="_blank">https://channels.istrav.com</a> // backend</li>
   </ul>
+  <h3>> scripts and documentation:</h3>
   <p>i've added event.sources() to the top because of it's importance. that's because we record every command and every query that goes on around the frontend and backend. so let's get stared...</p>
   <br />
   <div style="text-align: center;">
