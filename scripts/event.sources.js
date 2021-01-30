22a80de543085db1c338df2ecd67cc4ee55aad81
trabur <travis.burandt@gmal.com>
2021-01-30 12:45:16 -0600

    demo.payload

--- scripts/event.sources.js
+++ scripts/event.sources.js
@@ -4,9 +4,9 @@ export async function doEventSource (scriptId, folderId, stateId, sourceId, stor
   // always return an event object with these required props:
   return {
     id: window.id(32),                   // very long random string
-    startAt: Date.now(),                 // set by client
+    timeAt: Date.now(),                  // set by this function
     serverAt: null,                      // set by server
-    finishAt: null,                      // set by client
+    clientAt: null,                      // set by client
     folder: folderId || 'event.sources', // data relation
     script: scriptId || 'doEventSource', // cqrs
     payload: undefined,                  // undefined = command
@@ -21,11 +21,13 @@ export async function doEventSource (scriptId, folderId, stateId, sourceId, stor
 export async function doPublish (id, body) {
   // remember to always return an event object
   let es = await scripts.doEventSource('doPublish')
+  let demo = await scripts.doEventSource('doEventSource')
+  demo.payload = { hello: "world" }
 
   // make sure all arguements are saved to the event object
   es.arguements = {
     id: id || 'my-source',
-    body: body || { hello: "world" }
+    body: body || demo
   }
 
   // perform the doPublish thing
--- templates/dashboard.js
+++ templates/dashboard.js
@@ -2,8 +2,32 @@ export let dashboardTemplate = /*html*/`
   <h1>DASHBOARD</h1>
   <h3>> data sorcery for wizards!</h3>
   <p>this dashboard is a collection of tools and utilities put together for running a robust and reliable service business. the dashboard navigation sits on the left, the dashboard controls sits in the center, while the dashboard reports get streamed on the right.</p>
+  <div style="text-align: center;">
+    <a href="/scripts" class="waves-effect waves-light btn">scripts</a>
+    <a href="/templates" class="waves-effect waves-light btn">templates</a>
+  </div>
+  <p>i've placed the two buttons above for any devlopers, such as myself, to mainly browse and import code into other projects. these are our functions as a service API endpoints available for 3rd party or public use. for example, <a href="https://oldneckties.com" target="_blank">oldneckties.com</a> and <a href="https://printedbasics.com" target="_blank">printedbasics.com</a> are both storefronts with a cart and checkout which use these scripts & templates.</p>
+  <p>the rest of the project is also open source. you can clone all of our code and run each project by yourself; that is: if you know what your doing. although, i'm updating the code daily which would make constantly deploying to production a hassle therefore it's built with multi tenant in mind. i also have 10 or so other websites that i want to integrate without having to redeploy all kinds of other code and _blank_ as a service 3rd party software.</p>
+  <p>code involved:</p>
+  <ul>
+    <li>https://github.com/trabur/istrav-api</li>
+    <li>https://github.com/trabur/istrav-channels</li>
+    <li>https://github.com/trabur/istrav-dashboard</li>
+    <li>https://github.com/trabur/istrav-library</li>
+    <li>https://github.com/trabur/istrav-storefront</li>
+  </ul>
+  <p>speaking of 3rd party software here is a list of products that i use just to keep the lights on. note: all these services have free plans available however if i start getting use out of these endpoints i'll have to start paying and ultimatly having to charge subscribers.</p>
+  <p>services involved:</p>
+  <ul>
+    <li>aws.amazon.com // route53 domain name</li>
+    <li>cloudflare.com // dns</li>
+    <li>cloudamqp.com // rabbitmq as a service</li>
+    <li>cloud.mongodb.com // Database-as-a-Service for MongoDB</li>
+    <li>gigalixir.com // Elixir’s Platform as a Service</li>
+    <li>heroku.com // node.js frontend and backend</li>
+    <li>elephantsql.com // PostgreSQL as a Service</li>
+  </ul>
   <p>we've added event.sources() to the top because of it's importance. that's because we record a history of every command and every query that goes on around the frontend and backend. so let's get stared...</p>
-
   <br />
   <div style="text-align: center;">
     <a href="/event-sources" class="waves-effect waves-light btn">continue</a>
