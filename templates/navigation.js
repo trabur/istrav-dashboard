734702b84d60a0d8931a90b01e4fa444a8ba8b40
trabur <travis.burandt@gmal.com>
2021-02-03 14:21:35 -0600

    tenant-apps

--- templates/navigation.js
+++ templates/navigation.js
@@ -3,6 +3,7 @@ export let navigationTemplate = /*html*/`
   <li><a href="#" onclick="onNavItemClick('/event-sources'); return false;">event.sources()</a></li>
   <li><a href="#" onclick="onNavItemClick('/event-backup'); return false;">event.backup()</a></li>
   <li><a href="#" onclick="onNavItemClick('/event-logging'); return false;">event.logging()</a></li>
+  <li><a href="#" onclick="onNavItemClick('/tenant-apps'); return false;">tenant.apps()</a></li>
   <li><a href="#" onclick="onNavItemClick('/account-users'); return false;">account.users()</a></li>
   <li><a href="#" onclick="onNavItemClick('/account-cart'); return false;">account.cart()</a></li>
   <li><a href="#" onclick="onNavItemClick('/account-orders'); return false;">account.orders()</a></li>
@@ -12,7 +13,6 @@ export let navigationTemplate = /*html*/`
   <li><a href="#" onclick="onNavItemClick('/fleet-locations'); return false;">fleet.locations()</a></li>
   <li><a href="#" onclick="onNavItemClick('/store-featured'); return false;">store.featured()</a></li>
   <li><a href="#" onclick="onNavItemClick('/store-categories'); return false;">store.categories()</a></li>
-  <li><a href="#" onclick="onNavItemClick('/store-collections'); return false;">store.collections()</a></li>
   <li><a href="#" onclick="onNavItemClick('/store-products'); return false;">store.products()</a></li>
 </ul>
 `
