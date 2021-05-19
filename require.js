11688e86fe95716e9fe405f4ae07fa5d5fadf94f
istrav <travis.burandt@gmail.com>
2021-05-19 18:57:17 -0500

    commonjs

--- require.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * channels
- */
-var Socket = require("./node_modules/phoenix/priv/static/phoenix.js").Socket
-var w3cwebsocket = require("./node_modules/websocket/lib/websocket.js").w3cwebsocket
-
-window.socket = new Phoenix.Socket("wss://channels.istrav.com/socket", {transport: w3cwebsocket})
